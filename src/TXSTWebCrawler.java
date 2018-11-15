50867ce3cb8b464f1e1c6c9c981666c9b21b24b5
Elizabeth Phillips <ELIZABETH_PHILLIPS@homedepot.com>
2018-11-14 18:02:27 -0600

    Initial commit

---
+++ src/TXSTWebCrawler.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class TXSTWebCrawler {
+
+    private static final int MAX_PAGES_TO_SEARCH = 10;
+    private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit = new LinkedList<String>();
+
+    public static void main(String[] args){
+
+    }
+}
