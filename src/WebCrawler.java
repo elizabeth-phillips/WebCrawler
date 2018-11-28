ddb78a5beaa406d383c642ff7db5bc359229972b
Elizabeth Phillips <ELIZABETH_PHILLIPS@homedepot.com>
2018-11-28 00:21:52 -0600

    Updated Webcrawler to DFS

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,70 +8,144 @@ import org.jsoup.select.Elements;
 
 
 public class WebCrawler {
-    private static ArrayList<String> processedUrls = new ArrayList<>();
-    private static String mainPage = "https://cs.txstate.edu";
-    private static String phrase = "about_us";
-    private static TreeNode<String> root = new TreeNode<String>(mainPage);
-    private static int count = 1;
+    private String phraseToSearchFor;
+    private TreeNode homepage;
+    private Set<String> pagesVisited = new HashSet<>();
+    private List<TreeNode> pagesToVisit = new ArrayList<>();
+    private ArrayList< ArrayList<TreeNode>> depths = new ArrayList<>();
+    private int currDepth;
+    private int MAX_DEPTH;
 
-    public static void main(String[] args) {
-        processPage(root);
+    public WebCrawler(String homePage, String searchingFor, int MAX_DEPTH){
+        this.homepage = new TreeNode(homePage);
+        this.pagesToVisit.add(this.homepage);
+        depths.add(new ArrayList<TreeNode>(){});
+        depths.get(this.homepage.getDepth()).add(this.homepage);
+        this.phraseToSearchFor = searchingFor;
+        this.MAX_DEPTH = MAX_DEPTH;
     }
 
-    private static void processPage(TreeNode<String> currURL){
-        //check if the given mainURL is already in database
-        boolean match = currURL.getChildren().contains(currURL.data);
+    public void search(){
+        do{
+            processPage(pagesToVisit.get(0));
+        }
+        while(pagesToVisit.get(0).getDepth() <= MAX_DEPTH && pagesToVisit.size() != 0);
+
+        System.out.println(String.format("Now have visited %s links! %s links left to visit", pagesVisited.size(), (pagesToVisit.size()-pagesVisited.size())));
+        processPage(pagesToVisit.get(0));
+        System.out.println(depths);
+
+
+        currDepth = -1;
+        printTree();
+    }
+
+    private void processPage(TreeNode currURL){
+        if(!currURL.equals(this.homepage)) {
+            System.out.println(String.format("parent -> %s link -> %s depth -> %s", currURL.getParent().getData(), currURL.getData(), currURL.getDepth()));
+        }
+        boolean match = currURL.getChildrenData().contains(currURL.getData());   // Stops cyclical dependencies
 
         if(!match){
             try {
-                Document doc = Jsoup.connect(currURL.data).get();
-                if(doc.text().contains(phrase)){
-                    System.out.println(String.format("Document: %s",currURL.data));
-                }
+                Document doc = Jsoup.connect(currURL.getData()).get();
 
-                Elements questions = doc.select("a[href]");
+                Elements questions = doc.select("a[href]");                     // gets all links
+
                 for(Element link: questions){
-                    if(link.attr("href").contains(phrase)) {
-                        String nodeURL = link.attr("abs:href");
-                        if(!currURL.getChildren().contains(nodeURL)) {
-                            TreeNode<String> currNode = currURL.addChild(nodeURL);
-                            System.out.println(currNode.data);
-                            processPage(currNode);
-                        }
+                    String nodeURL = link.attr("abs:href");
+                    if(nodeURL.contains(this.phraseToSearchFor) && !currURL.getChildrenData().contains(nodeURL)) {                                   // If the link contains the current searchingFor
+                        currURL.addChild(nodeURL);     // Makes the current link into a Tree Node
                     }
                 }
             }
             catch(IOException ex){
-                System.out.println(ex.getMessage());
+//                System.out.println(ex.getMessage());
+            }
+        }
+        pagesToVisit.addAll(currURL.getChildren());
+        pagesToVisit.remove(currURL);
+        pagesVisited.add(currURL.getData());
+    }
+
+
+    private void printTree(){
+        for(int i = 0; i < depths.size(); i++){
+            System.out.println("====> Depth" + i);
+            for(TreeNode currNode: depths.get(i)){
+                System.out.println(String.format("%s -> %s -> %s", (currNode.getParent() == null) ? "no parent" : currNode.getParent().getData(), currNode.getData(), currNode.getChildrenData()));
             }
         }
     }
 
 
+    private ArrayList<TreeNode> getAllFromDepth(int depth){
+        TreeNode temp = this.homepage;
+        while(temp.getDepth() != depth){
+            temp = temp.get
+        }
+    }
 
-    public static class TreeNode<T> {
-        T data;
-        TreeNode<T> parent;
-        List<TreeNode<T>> children;
+    private class TreeNode {
+        String data;
+        TreeNode parent;
+        Set<TreeNode> children;
+        ArrayList<String> childrenData;
+        int depth = 0;
 
-        private TreeNode(T data) {
+
+        private TreeNode(String data) {
             this.data = data;
-            this.children = new LinkedList<TreeNode<T>>();
+            this.children = new HashSet<>();
+            this.childrenData = new ArrayList<>();
         }
 
-        private TreeNode<T> addChild(T child) {
-            TreeNode<T> childNode = new TreeNode<T>(child);
-            childNode.parent = this;
+        private TreeNode addChild(String child) {
+            TreeNode childNode = new TreeNode(child);
+            childNode.setDepth(this.depth+1);
+            childNode.setParent(this);
+            if(childNode.getDepth() == depths.size()){
+                depths.add(new ArrayList<TreeNode>(){});
+            }
+
+            depths.get(childNode.getDepth()).add(childNode);
             this.children.add(childNode);
+            this.childrenData.add(child);
             return childNode;
         }
 
-        private List<T> getChildren(){
-            List<T> childrenList = new LinkedList<T>() ;
-            for(TreeNode<T> child: this.children){
-                childrenList.add(child.data);
-            }
-            return childrenList;
+
+        private Set<TreeNode> getChildren() {
+            return children;
         }
+
+        private String getData() {
+            return data;
+        }
+
+        private void setData(String data) {
+            this.data = data;
+        }
+
+        private TreeNode getParent() {
+            return parent;
+        }
+
+        private void setParent(TreeNode parent) {
+            this.parent = parent;
+        }
+
+        private ArrayList<String> getChildrenData() {
+            return childrenData;
+        }
+
+        private int getDepth() {
+            return depth;
+        }
+
+        private void setDepth(int depth) {
+            this.depth = depth;
+        }
+
     }
 }
---
+++ src/WebCrawlerTester.java
@@ -0,0 +1,10 @@
+
+public class WebCrawlerTester {
+
+    public static void main(String[] args) {
+        String mainPage = "https://cs.txstate.edu";
+        String searchingFor = "https://cs.txstate.edu/about_us/";
+        WebCrawler search = new WebCrawler(mainPage, searchingFor, 3);
+        search.search();
+    }
+}
